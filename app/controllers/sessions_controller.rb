3180100a9a2a6ea5b7327bbe232aa2743107251c
Jon Perkins <jjperk@gmail.com>
2016-04-26 01:35:25 +0000

    Add login, logout features

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      remember user
+      redirect_to login_path
+      flash[:success] = "You're logged in!"
+    else 
+      flash[:danger] = "Invalid email/password combination"
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url
+    flash[:success] = "You're logged out!"
+  end
+  
 end
